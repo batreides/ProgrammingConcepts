336220a546fe38d3ac32664bbd344719850af968
dhyan c <www.dhyanchand09@gmail.com>
2025-04-28 23:16:57 +0530

    Add files via upload

---
+++ Patterns-I/halfPyramidPattern.java
@@ -0,0 +1,13 @@
+public class halfPyramidPattern {
+  public static void main(String[] args) {
+    int i;
+    int j;
+    for(i=1; i<=4; i++){
+      for(j=1; j<=i; j++){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
+//self-u made-u shehzaaadaaaa
